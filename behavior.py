d86daa49f4fac0f9888ad7e9aecaacf581658f81
Yuxia <yuxia.cheng@deakin.edu.au>
2016-09-22 12:29:54 +1000

    22 morning update

--- behavior.py
+++ behavior.py
@@ -3,12 +3,16 @@
 from __future__ import print_function
 import threading,Queue,time,os,sys
 import libvirt
+import numpy as np
 from xml.etree import ElementTree
+from numpy import array
+from sklearn.cluster import KMeans
 
-class Monitor(threading.Thread):
+class NFVMonitor(threading.Thread):
    """performance monitoring class"""
    def __init__(self,t_name,domID,queue):
       self.queue = queue
+      self.__running = True
 
       conn = libvirt.open('qemu:///system')
       if conn == None:
@@ -20,6 +24,9 @@ class Monitor(threading.Thread):
          exit(1)
 
       threading.Thread.__init__(self, name=t_name)
+   
+   def terminate(self):
+      self._running = False
 
    def getCPUstats(self):
       return self.dom.getCPUStats(True)
@@ -36,7 +43,7 @@ class Monitor(threading.Thread):
       return self.dom.interfaceStats(iface)
 
    def startMonitor(self):
-      while True:
+      while True and self.__running:
          cpu_stats = self.getCPUstats()
          mem_stats = self.getMEMstats()
          rd_req, rd_bytes, wr_req, wr_bytes, err = self.getDISKstats()
@@ -56,12 +63,32 @@ class Monitor(threading.Thread):
          time.sleep(2) 
 
    def run(self):
-	self.startMonitor()
+      self.startMonitor()
 
+class NFVCluster(threading.Thread):
+   """transform VNF observations into a sequence of cluster labels"""
+   def __init__(self,t_name):
+      self.__running = True
+      threading.Thread.__init__(self, name=t_name)
+   
+   def terminate(self):
+      self.__running = False
 
+   def startCluster():
+      features = np.loadtxt('features.txt')
+      print (features)
+      labels = open('/home/stack/labels.txt','w')
+      labels.close()
+   
+   def run(self):
+      self.startCluster()
 
-features = open('/home/stack/features.txt','a')
+features = open('/home/stack/features.txt','w')
 q = Queue.Queue()
-nfv_monitor = Monitor('nfv_monitor',33,q)
+nfv_monitor = NFVMonitor('nfv_monitor',33,q)
 nfv_monitor.start()
+time.sleep(20)
+nfv_monitor.terminate()
 nfv_monitor.join()
+features.close()
+
--- test.py
+++ test.py
@@ -2,11 +2,23 @@
 #coding=utf8
 
 import os,sys,time
+import numpy as np
+#from sklearn import hmm
+startprob = np.array([0.6,0.3,0.1])
+transmat = np.array([[0.7,0.2,0.1],[0.3,0.5,0.2],[0.3,0.3,0.4]])
+means = np.array([[0.0,0.0],[3.0,-3.0],[5.0,10.0]])
+covars = np.tile(np.identity(2),(3,1,1))
+#model = hmm.GaussianHMM(3, "full", startprob, transmat)
 
-for i in range(0,7):
-   time.sleep(1)
-   print i
-   f = open('/home/stack/testwrite','a')
-   f.write('hello\n')
-   f.write('world\n')
-   f.close()
+from sklearn.cluster import KMeans
+from sklearn import datasets
+
+estor = {'k_means_3': KMeans(n_clusters=3)}
+iris = datasets.load_iris()
+X = iris.data
+Y = iris.target
+print X
+for name, est in estor.items():
+   est.fit(X)
+   print est.labels_
+
