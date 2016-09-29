0e21f017809b823471027c590c3d066d7151a717
Yuxia <yuxia.cheng@deakin.edu.au>
2016-09-29 16:50:57 +1000

    add program analysis module

---
+++ program.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2.7
+#encoding:utf-8
+import os,sys
+#pip install editdistance
+#https://pypi.python.org/pypi/editdistance
+#amoco construct cfg, install from source code
+#https://github.com/bdcht/amoco
+import amoco,editdistance
+
+class NFVInitial():
+"""VNF program initial analysis """
+   def __init__(self):
+      
+
+class NFVUnpack():
+""""""
+
+class NFVSignature():
+
+class NFVClassify(): 
+
+
+if __name__ == "__main__":
+
